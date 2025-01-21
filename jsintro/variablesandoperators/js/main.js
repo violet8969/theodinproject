d5bf87998c1de9d1170c6492728f446e27e7c783
violet8969 <155793460+violet8969@users.noreply.github.com>
2025-01-20 19:21:12 -0500

    did several js intro exercises and created folders

---
+++ jsintro/variablesandoperators/js/main.js
@@ -0,0 +1,24 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+// age = 54;
+ 
+const pi = 3.14;
+
+
+console.log(pi); 
+
+
+let  x, y, z
+
+x = 5
+y =2 
+z = x % y
+
+alert(z)
